1483d4641575deacc712faf71210442583995a02
RaphaelCouillaud <couillaud.raphael@gmail.com>
2020-12-23 00:08:31 +0100

    modif createMmessage controllers

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -11,13 +11,13 @@ const fs = require ('fs'); // Création et gestion des fichiers //
 // Création d'un message //
 exports.createMessage = (req, res, next) => {
     const messageObject = JSON.parse(req.body.message); // Extraction de l'objet JSON //
-    delete messageObject.id;
+    delete messageObject.id; // on retire le champ id du corps de la requête//
     const message = new Message({
-        ...messageObject,
+        ...messageObject, //...raccourci pour prendre en compte tous les éléments du body de la requête//
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`, // Protocole, nom d'hôte, images, nom du fichier //        
     });
     message.save()
-        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        .then(() => res.status(201).json({ message: 'Message enregistré !' }))
         .catch(error => res.status(400).json({ error }));
 };
 
@@ -28,7 +28,7 @@ exports.deleteMessage = (req, res, next) => {
             const filename = message.imageUrl.split('/images/')[1]; // Qd on le trouve, on extrait le nom du fichier //
             fs.unlink(`images/${filename}`, () => { // On le supprime avec fs.unlink //
                 Message.destroy({ id: req.params.id }) // Une fois la suppression, on l'indique à la base de données //
-                    .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                    .then(() => res.status(200).json({ message: 'Message supprimé !' }))
                     .catch(error => res.status(400).json({ error }));
             });
         })
