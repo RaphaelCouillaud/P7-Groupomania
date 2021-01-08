f8bea2d90f2f56309f9ef6fcf417cd7c6c704ae5
RaphaelCouillaud <couillaud.raphael@gmail.com>
2021-01-08 17:55:50 +0100

    helmet secure

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const helmet = require('helmet');
 
 // Importation des routeurs //
 const userRoutes = require("./routes/user"); // Importation de la route user //
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json()); // App requiert BodyParser //
-
+app.use(helmet());
 app.use('/images', express.static(path.join(__dirname, 'images'))); // Pour toute requête envoyée à /images/, on sert ce dossier statique image //
 
 // Enregistrement des routeurs //
